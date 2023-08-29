2b2daaef6f8a6e7de5c1a961cad94b0a4b230520
ercelserhat <ercelserhat@gmail.com>
2023-08-30 02:12:44 +0300

    Launchscreen and login page designed

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
 }
 
