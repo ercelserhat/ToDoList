8205b63dbe37e8316b76ca9929ff1cfb24dab682
ercelserhat <ercelserhat@gmail.com>
2023-08-30 17:09:06 +0300

    Register page designed

---
+++ TodoList/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.swift
+//  TodoList
+//
+//  Created by Serhat on 30.08.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func register(_ sender: Any) {
+    }
+}
