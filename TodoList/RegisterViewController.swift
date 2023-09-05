5cb036cf14b4d736a45b391107cc1745a508e881
ercelserhat <ercelserhat@gmail.com>
2023-09-05 03:05:26 +0300

    Updated register page

--- TodoList/RegisterViewController.swift
+++ TodoList/RegisterViewController.swift
@@ -12,6 +12,7 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordAgainTextField: UITextField!
@@ -21,20 +22,32 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func register(_ sender: Any) {
-        if emailTextField.text != "" && passwordTextField.text != ""{
-            if passwordTextField.text == passwordAgainTextField.text{
-                Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!){authdataresult, error in
-                    if error != nil{
-                        self.hataMesaji(titleInput: "HATA", messageInput: error?.localizedDescription ?? "Hata oluştu. Lütfen tekrar deneyin.")
-                    }else{
-                        self.performSegue(withIdentifier: "registerToMain", sender: nil)
+        if nameTextField.text != "" {
+            if emailTextField.text != "" && passwordTextField.text != ""{
+                if passwordTextField.text == passwordAgainTextField.text{
+                    Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!){authdataresult, error in
+                        if error != nil{
+                            self.hataMesaji(titleInput: "HATA", messageInput: error?.localizedDescription ?? "Hata oluştu. Lütfen tekrar deneyin.")
+                        }else{
+                            let firestoreDatabase = Firestore.firestore()
+                            let firestoreUser = ["name" : self.nameTextField.text!, "email" : self.emailTextField.text!, "tarih" : FieldValue.serverTimestamp()] as [String : Any]
+                            firestoreDatabase.collection("User").addDocument(data: firestoreUser){ error in
+                                if error != nil{
+                                    self.hataMesaji(titleInput: "HATA", messageInput: error?.localizedDescription ?? "Hata oluştu. Lütfen tekrar deneyin.")
+                                }else{
+                                    self.performSegue(withIdentifier: "registerToMain", sender: nil)
+                                }
+                            }
+                        }
                     }
+                }else{
+                    hataMesaji(titleInput: "HATA", messageInput: "Şifreler birbirine uymuyor!")
                 }
             }else{
-                hataMesaji(titleInput: "HATA", messageInput: "Şifreler birbirine uymuyor!")
+                hataMesaji(titleInput: "HATA", messageInput: "Email veya şifre boş olamaz!")
             }
         }else{
-            hataMesaji(titleInput: "HATA", messageInput: "Email veya şifre boş olamaz!")
+            hataMesaji(titleInput: "HATA", messageInput: "İsim alanı boş olamaz!")
         }
     }
     
